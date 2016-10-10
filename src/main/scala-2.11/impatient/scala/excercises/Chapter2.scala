245967a1aa423a0a8efc1371b595af97ecd0d845
Kirill Krolik <k.krolik@smartdesign.by>
2016-10-10 18:58:54 +0300

    implement 2.1

--- src/main/scala-2.11/impatient/scala/excercises/Chapter2.scala
+++ src/main/scala-2.11/impatient/scala/excercises/Chapter2.scala
@@ -2,6 +2,12 @@ package impatient.scala.excercises
 
 class Chapter2 {
 
-  def signum(n: Integer) = n
+  def signum(n: Integer) = {
+    if (n > 0)
+      1
+    else if (n < 0)
+      -1
+    else 0
+  }
 
 }
