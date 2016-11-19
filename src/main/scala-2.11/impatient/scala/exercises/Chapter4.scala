9577327322a82cdb1efb33957d03d8da0b78f06b
Kirill Krolik <yoursuffer@gmail.com>
2016-11-19 20:26:24 +0300

    write specs for 4.2

--- src/main/scala-2.11/impatient/scala/exercises/Chapter4.scala
+++ src/main/scala-2.11/impatient/scala/exercises/Chapter4.scala
@@ -1,9 +1,16 @@
 package impatient.scala.exercises
 
+import java.io.File
+import java.util.Scanner
+
 class Chapter4 {
 
   def produceGizmosMapWithDiscount(map: Map[String, Double]): Map[String, Double] = {
     map.map(tuple => (tuple._1, tuple._2 / 100 * 90))
+  }
+
+  def countWordsNumberInFile(file: File)(implicit scanner: Scanner = new Scanner(file)): Map[String, Int] = {
+    Map()
   }
 
 }
--- src/test/scala-2.11/impatient/scala/exercises/Chapter4Spec.scala
+++ src/test/scala-2.11/impatient/scala/exercises/Chapter4Spec.scala
@@ -1,5 +1,8 @@
 package impatient.scala.exercises
 
+import java.io.File
+import java.util.Scanner
+
 import impatient.scala.utils.random.Random
 import org.specs2.ScalaCheck
 import org.specs2.mock.Mockito
@@ -17,6 +20,28 @@ class Chapter4Spec extends Specification with ScalaCheck with Mockito {
 
       subject.produceGizmosMapWithDiscount(input) mustEqual expected
 
+    }
+
+  }
+
+  "words frequency in a file calculator" should {
+
+    "handle empty file gracefully" in {
+      val file = mock[File]
+      val scanner = mock[Scanner]
+      scanner.hasNext() returns false
+
+      subject.countWordsNumberInFile(file)(scanner) mustEqual Map()
+    }
+
+    "correctly count number of words" in {
+      val file = mock[File]
+      val scanner = mock[Scanner]
+
+      scanner.hasNext() returns true thenReturns true thenReturns false
+      scanner.next() returns "foo" thenReturns "bar" thenReturns "foo"
+
+      subject.countWordsNumberInFile(file)(scanner) mustEqual Map("foo" -> 2, "bar" -> 1)
     }
 
   }
